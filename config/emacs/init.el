881ecfef6b4988fd8b4adc78d6f3509ff119d808
Bastian Hussi <bastian@ipfso.de>
2021-03-16 22:56:57 +0100

    Removed unnecessary bindings, improved the leader key and finally understood display-buffer-base-action.

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -2,7 +2,10 @@
 ;; init.el --- Where it all begins...
 
 (org-babel-load-file "config.org")
-(org-babel-load-file "private.org")
+
+(defvar my/private-config "private.org")
+(when (file-exists-p my/private-config)
+  (org-babel-load-file my/private-config))
 
 ;; set the default-directory to $HOME.
 (cd "~/")
